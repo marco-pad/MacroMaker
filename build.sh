e8473edbc4e73dee5514e5568feb425614bee891
let <timbukreew624@gmail.com>
2024-03-21 07:00:37 +0100

    updated build script

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cargo build -p macromaker --release
 cargo +nightly build -p firmware --release --target riscv64gc-unknown-linux-musl -Zbuild-std=core,std,panic_abort
-scp -O target/riscv64gc-unknown-linux-musl/release/firmware root@milkv:programs/firmware
+scp -O target/riscv64gc-unknown-linux-musl/release/firmware root@192.168.42.1:programs/firmware
